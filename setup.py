aacd1ec3241304bbbc2c64abbff0810b62d31571
fabiobatalha <fabiobatalha@gmail.com>
2016-08-26 15:21:44 -0300

    Ajustes em Dockerfile

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requires = []
 
 setup(
     name="access",
-    version='0.3.4',
+    version='0.4.4',
     description="A SciELO RPC server and API to retrieve access statistics from the SciELO Network ",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
